b9a101e0298221a53a4a9ccb4f410ea8c2783426
unknown <13523067@students.uii.ac.id>
2020-12-02 14:40:34 +0700

    mapan.

---
+++ src/main/java/com/carikerja/mapan/MapanApplication.java
@@ -0,0 +1,13 @@
+package com.carikerja.mapan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carikerja/mapan/MapanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carikerja.mapan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
